1be2eedce38b05d3559948dbbac7ea4129f9d4d6
leecoders <als-1994@hanmail.net>
2019-10-01 15:38:12 +0900

    refactor: template는 render에서 그려주는 것으로 변경

--- server/public/src/components/Header/Header.js
+++ server/public/src/components/Header/Header.js
@@ -1,5 +1,4 @@
-import headerTemplate from "./template.js";
-import { $, toggleUserClickState } from "../../utils/util.js";
+import { $ } from "../../utils/util.js";
 
 class Header {
   constructor(parentElement, pageName) {
@@ -8,7 +7,15 @@ class Header {
     this.render();
   }
   render() {
-    this.parentElement.innerHTML = headerTemplate;
+    this.parentElement.innerHTML = /*html*/ `
+      <span>TODO</span>
+      <div class="menu-wrapper">
+      </div>
+      <div class="user-wrapper">
+          <button id="user-button" type="button"></button>
+          <div class="user-bubble-container"></div>
+      </div>
+    `;
   }
 }
 
--- server/public/src/components/Header/template.js
+++
@@ -1,9 +0,0 @@
-export default /*html*/ `
-<span>TODO</span>
-<div class="menu-wrapper">
-
-</div>
-<div class="user-wrapper">
-    <button id="user-button" type="button"></button>
-    <div class="user-bubble-container"></div>
-</div>`;
