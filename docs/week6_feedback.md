248db682556a7d7e80aded82a1a60842e3052022
leecoders <als-1994@hanmail.net>
2019-10-07 17:50:56 +0900

    feat: 투두 카드 드래그
    setTodoDragEvent() 메서드 내에서 mousedown, mousemove, mouseup을 통해 드래그 UI 구현 -> mousedown 시 해당 위치에 todo card 있으면 복사해서 그려넣음 -> 시작점에 대해 마우스 좌표, 투두 카드 왼쪽 상단 좌표, 투두 카드, 드래그 타겟을 Section 멤버 변수에 저장 -> move 시 변위값 만큼 투두 카드 시작점을 기준으로 복사한 엘리먼트에 대해 이동시켜줌 -> mouseup 시 드래그 타겟이 있으면 삭제

--- docs/week6_feedback.md
+++ docs/week6_feedback.md
@@ -3,3 +3,9 @@
 ### CSS) `overflow-x`, `overflow-y` 속성을 통해 가로, 세로에 대해 따로 처리할 수 있다.
 
 ### CSS) `min-width`, `min-height` 속성을 통해 동적으로 변하는(예를 들어 textarea)에 대해 최소 크기를 지정할 수 있다.
+
+### CSS) drag & drop 구현 시 원래 위치의 element의 왼쪽 상단 자리에 복사하기
+- `getBoundingClientRect().x`와 `getBoundingClientRect().y`를 통해 절대 좌표를 구할 수 있다.
+- 새로 복사하려는 element 또한 왼쪽 상단을 기준으로 만들어진다.
+- 여기서 주의해야 할 점은 element에 `margin`이 있을 때, 기준 좌표를 기준으로 `margin`이 먹는다.
+  - `margin` 만큼 보정하던지 `margin`을 빼던지 하면 된다.
--- server/public/src/components/Section/Section.js
+++ server/public/src/components/Section/Section.js
@@ -10,6 +10,7 @@ class Section {
     this.render();
     this.setLists();
     this.setTodoDragEvent();
+    this.setListMouseEnterEvent();
   }
 
   setLists() {
@@ -19,13 +20,46 @@ class Section {
     }
   }
 
+  // setListMouseEnterEvent() {
+  //   $("section").addEventListener("mouseenter", e => {
+  //     const target = e.target;
+  //     const list = findAncestorsElement(target, "todo-container");
+  //     // if (list.className !== "todo-container") return;
+  //     console.log(list);
+  //   });
+  // }
+
   setTodoDragEvent() {
     $("section").addEventListener("mousedown", e => {
       const target = e.target;
-      console.log(findAncestorsElement(target, "todo-wrapper"));
-      // if (target.className === "todo-wrapper") {
-      //   this.dragTarget = target;
-      // }
+      const todo = findAncestorsElement(target, "todo-wrapper");
+      if (todo.className !== "todo-wrapper") return;
+      const todoX = todo.getBoundingClientRect().x;
+      const todoY = todo.getBoundingClientRect().y;
+      this.dragStartX = e.clientX; // 마우스 시작 좌표
+      this.dragStartY = e.clientY;
+      this.dragTodoX = todoX;
+      this.dragTodoY = todoY - 7.5;
+      this.dragTarget = todo.cloneNode(true);
+      this.dragTarget.style.left = this.dragTodoX + "px";
+      this.dragTarget.style.top = this.dragTodoY + "px"; // margin-top 보정
+      this.dragTarget.style.position = "fixed";
+      this.dragTarget.style.opacity = "0.9";
+      this.todo = todo;
+      todo.style.opacity = "0.5";
+      $("section").appendChild(this.dragTarget);
+    });
+    $("section").addEventListener("mousemove", e => {
+      if (!this.dragTarget) return;
+      const relativeX = e.clientX - this.dragStartX;
+      const relativeY = e.clientY - this.dragStartY;
+      this.dragTarget.style.left = this.dragTodoX + relativeX + "px";
+      this.dragTarget.style.top = this.dragTodoY + relativeY + "px";
+    });
+    $("section").addEventListener("mouseup", e => {
+      if (!this.dragTarget) return;
+      this.dragTarget.remove();
+      this.dragTarget = undefined;
     });
   }
 
