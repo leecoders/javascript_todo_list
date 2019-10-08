d1ef7442708157bae55cf30f66922292bb699ad8
leecoders <als-1994@hanmail.net>
2019-10-08 13:43:18 +0900

    fix: 스크롤바 브라우저 호환성 이슈
    맥 최신 버전에서는 스크롤 바가 absolute로 생기지만 윈도우나 맥 구버전에서는 relative로 생겨 카드가 밀리는 현상 -> overflow-y: auto로 해결

--- server/public/src/components/Todo/Todo.js
+++ server/public/src/components/Todo/Todo.js
@@ -11,6 +11,7 @@ class Todo {
     return $(`#todo-wrapper-${this.todoIdx}`);
   }
 
+  // this.todoIdx가 아닌 DB에 저장된 실제 todo 카드의 아이디를 DOM 아이디로 사용하도록 변경할 필요가 있음
   render() {
     this.parentElement.insertAdjacentHTML(
       "beforeend",
@@ -18,10 +19,10 @@ class Todo {
       <div id="todo-wrapper-${this.todoIdx}" class="todo-wrapper">
         <div class="todo-text-image"></div>
         <div class="todo-content-container">
-          <div id="todo-content-${this.todoIdx}" class="todo-content">sdfsasdfdsaklfhjsdlajflkdsjflksdjflkdf</div>
+          <div class="todo-content">sdfsasdfdsaklfhjsdlajflkdsjflksdjflkdf</div>
           <div class="todo-info-wrapper">
             <span class="added-by">Added by</span>
-            <span id="added-by-id-${this.todoIdx}" class="added-by-id">leecoders</span>
+            <span class="added-by-id">leecoders</span>
           </div>
         </div>
         <div id="todo-delete-button-${this.todoIdx}" class="todo-delete-button"></div>
