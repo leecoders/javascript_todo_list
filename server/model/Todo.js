36c8fb22d4f67eddd2d311d379b2e7ed15ee80d1
leecoders <als-1994@hanmail.net>
2019-10-08 14:37:26 +0900

    feat: model/Todo.js 클래스 생성
    getBoardsByUserId, getListsByBoardId, getTodosByListId DB 접근 메서드 정의

---
+++ server/model/Todo.js
@@ -0,0 +1,52 @@
+const pool = require("./db.js");
+
+class Todo {
+  constructor() {}
+
+  async getBoardsByUserId(userId, res) {
+    try {
+      const connection = await pool.getConnection(async conn => conn);
+      const [rows] = await connection.query(
+        `select BOARD_ID, BOARD_NAME, BOARD_WRITE_PERMISSION, BOARD_READ_PERMISSION from BOARD where BOARD_OWNER=?`,
+        [userId]
+      );
+      connection.release();
+      if (rows.length) return res.json(rows);
+      else res.json({ message: "failure" });
+    } catch (err) {
+      res.json({ message: "db not connected" });
+    }
+  }
+
+  async getListsByBoardId(boardId, res) {
+    try {
+      const connection = await pool.getConnection(async conn => conn);
+      const [rows] = await connection.query(
+        `select LIST_ID, LIST_NAME from BOARD where LIST_BELONG_BOARD=?`,
+        [boardId]
+      );
+      connection.release();
+      if (rows.length) return res.json(rows);
+      else res.json({ message: "failure" });
+    } catch (err) {
+      res.json({ message: "db not connected" });
+    }
+  }
+
+  async getTodosByListId(ListId, res) {
+    try {
+      const connection = await pool.getConnection(async conn => conn);
+      const [rows] = await connection.query(
+        `select TODO_ID, TODO_CONTENT, TODO_BELONG_LIST, TODO_ADDED_BY from TODO where TODO_BELONG_LIST=?`,
+        [ListId]
+      );
+      connection.release();
+      if (rows.length) return res.json(rows);
+      else res.json({ message: "failure" });
+    } catch (err) {
+      res.json({ message: "db not connected" });
+    }
+  }
+}
+
+module.exports = Todo;
