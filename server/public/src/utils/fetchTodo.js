ddc656cdd12eaed4405ce93f62d1b0d78198b159
leecoders <als-1994@hanmail.net>
2019-10-08 14:44:08 +0900

    feat: frontend) todo 관련 fetch 작성

--- server/public/src/utils/fetchTodo.js
+++ server/public/src/utils/fetchTodo.js
@@ -12,4 +12,45 @@ const fetchUserInfo = () => {
     .catch(error => "error");
 };
 
-export { fetchUserInfo };
+const fetchBoardsByUserId = () => {
+  return fetch(serverUrl + "todo/get-boards", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(response => response)
+    .catch(error => "error");
+};
+
+const fetchListsByBoardId = () => {
+  return fetch(serverUrl + "todo/get-lists", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(response => response)
+    .catch(error => "error");
+};
+
+const fetchTodosByListId = () => {
+  return fetch(serverUrl + "todo/get-todos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(response => response)
+    .catch(error => "error");
+};
+
+export {
+  fetchUserInfo,
+  fetchBoardsByUserId,
+  fetchListsByBoardId,
+  fetchTodosByListId
+};
